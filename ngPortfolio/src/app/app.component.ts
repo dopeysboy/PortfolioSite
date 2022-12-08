de02f1c8b246676ba55b20562fd3abe248206c53
Tyler Tanner <tyler.j.tanner2@gmail.com>
2022-12-08 14:14:56 -0600

    Added components for navbar, footer, and home page, added style elements

--- ngPortfolio/src/app/app.component.ts
+++ ngPortfolio/src/app/app.component.ts
@@ -7,4 +7,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ngPortfolio';
+
+  github = 'https://www.github.com/dopeysboy';
+  linkedin = 'https://www.linkedin.com/in/tyler-j-tanner';
+  email = 'tyler.j.tanner2@gmail.com';
 }
